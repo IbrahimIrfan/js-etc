6e0204bdbea3b981b8a8fd18ffc1a7f179f86634
yingninggui <yingning.gui@uwaterloo.ca>
2019-06-08 11:42:17 -0400

    newline

--- icy-bot.cpp
+++ icy-bot.cpp
@@ -71,7 +71,6 @@ int main(int argc, char *argv[])
     vector<string> data;
     data.push_back(string("HELLO"));
     data.push_back(config.team_name);
-    data.push_back("\n");
 
     /*
       A common mistake people make is to conn.send_to_exchange() > 1
